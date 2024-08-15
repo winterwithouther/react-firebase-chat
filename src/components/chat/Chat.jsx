49e5bf19c096f994435173a04bbef1929a084149
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-08-14 20:58:08 -0400

    styled bottom section of chat and added functionality to emoji picker and input

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,36 +1,58 @@
 import "./Chat.css"
-import React from 'react'
+import EmojiPicker from "emoji-picker-react"
+import React, { useState } from 'react'
 
 export default function Chat() {
-  return (
-    <div className="chat">
-        <div className="top">
-            <div className="user">
-                <img src="./avatar.png" alt=""></img>
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Lorem Ipsum dolor, sit amet.</p>
+
+    const [open, setOpen] = useState(false)
+    const [text, setText] = useState("")
+
+    function handleEmoji(e) {
+        setText(prev => prev + e.emoji)
+        setOpen(false)
+    }
+
+    return (
+        <div className="chat">
+            <div className="top">
+                <div className="user">
+                    <img src="./avatar.png" alt=""></img>
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>Lorem Ipsum dolor, sit amet.</p>
+                    </div>
+                </div>
+                <div className="icons">
+                    <img src="./phone.png"></img>
+                    <img src="./video.png"></img>
+                    <img src="./info.png"></img>
                 </div>
             </div>
-            <div className="icons">
-                <img src="./phone.png"></img>
-                <img src="./video.png"></img>
-                <img src="./info.png"></img>
+            <div className="center">
+
+                
+            </div>
+            <div className="bottom">
+                <div className="icons">
+                    <img src="./img.png"></img>
+                    <img src="./camera.png"></img>
+                    <img src="./mic.png"></img>
+                </div>
+                <input 
+                    type="text" 
+                    placeholder="Type a message..." 
+                    onChange={(e) => setText(e.target.value)} 
+                    value={text} 
+                ></input>
+                <div className="emoji">
+                    <img src="./emoji.png" alt=""
+                    onClick={() => setOpen(prev => !prev)}></img>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                    </div>
+                </div>
+                <button className="send-button">Send</button>
             </div>
         </div>
-        <div className="center"></div>
-        <div className="bottom">
-            <div className="icons">
-                <img src="./img.png"></img>
-                <img src="./camera.png"></img>
-                <img src="./microphone.png"></img>
-            </div>
-            <input type="text" placeholder="Type a message..."></input>
-            <div className="emoji">
-                <img src="./emoji.png" alt=""></img>
-            </div>
-            <button className="send-button">Send</button>
-        </div>
-    </div>
-  )
+    )
 }
