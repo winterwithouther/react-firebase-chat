813061ac89f37c544008f22bada5a0d01db18f24
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-08-15 01:24:15 -0400

    styled detail profile section side

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,6 +3,48 @@ import React from 'react'
 
 export default function Detail() {
   return (
-    <div className="detail">Detail</div>
+    <div className="detail">
+        <div className="user">
+            <img src="./avatar.png" alt=""></img>
+            <h2>Jane Doe</h2>
+            <p>Lorem Ipsum Dolor sit amet</p>
+        </div>
+        <div className="info">
+            <div className="option">
+                <div className="title">
+                    <span>Chat Settings</span>
+                    <img src="./arrowUp.png" alt=""></img>
+                </div>
+            </div>
+            <div className="option">
+                <div className="title">
+                    <span>Privacy & help</span>
+                    <img src="./arrowUp.png" alt=""></img>
+                </div>
+            </div>
+            <div className="option">
+                <div className="title">
+                    <span>Shared Photos</span>
+                    <img src="./arrowDown.png" alt=""></img>
+                </div>
+                <div className="photos">
+                    <div className="photoItem">
+                        <div className="photoDetail">
+                            <img src="https://practicaltyping.com/wp-content/uploads/2020/09/vegeta2.jpg" alt=""></img>
+                            <span>Photo.png</span>
+                        </div>
+                        <img src="./download.png" className="icon" alt=""></img>
+                    </div>
+                </div>
+            </div>
+            <div className="option">
+                <div className="title">
+                    <span>Shared Files</span>
+                    <img src="./arrowUp.png" alt=""></img>
+                </div>
+            </div>
+            <button>Block User</button>
+        </div>
+    </div>
   )
 }
